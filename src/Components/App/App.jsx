a49eeaf400f515d33dc04eca3e123763a5939516
aditiverma1009 <aditiverma1009@gmail.com>
2020-01-19 16:39:28 +0530

    use functional component instead of a class component remove demo purpose lifecycle methods useState use to create and alter list

--- src/Components/App/App.jsx
+++ src/Components/App/App.jsx
@@ -1,90 +1,44 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import blogImage from '../../assets/rectangle.png';
 import BlogCard from '../BlogCard/BlogCard';
 import HomePage from '../HomePage/HomePage';
 import './App.scss';
 
-class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      allBlogs: [],
-    };
-  }
+function App() {
+  const [allBlogs, setAllBlogs] = useState([]);
 
-  componentWillMount = () => { // deprecated
-    console.log('component will mount');
-  };
-
-  componentDidMount = () => {
-    console.log('component did mount');
-  };
-
-  componentWillReceiveProps=() => { // deprecated
-    console.log('component will receive props');
-  }
-
-  shouldComponentUpdate=(nextProps, nextStates) => {
-    console.log('should Component Update');
-    return true;
-  }
-
-  componentWillUpdate=() => { // deprecated
-    console.log('component will update');
-  }
-
-  componentDidUpdate=(prevProps, prevState) => {
-    console.log('component did update', prevProps, prevState, this.state);
-  }
-
-  componentWillUnmount=() => {
-    console.log('component will unmount');
-  }
-
-  onFormSubmit = (event, content) => {
-    const { title, desc, imageurl } = content;
-    const { allBlogs } = this.state;
+  const onFormSubmit = (event, content) => {
+    const { title, desc, imageURL } = content;
     event.preventDefault();
-    this.setState({
-      allBlogs: [
-        ...allBlogs,
-        {
-          title,
-          imageUrl: imageurl,
-          imageAlt: title,
-          content: desc,
-        },
-      ],
-    });
+    setAllBlogs([...allBlogs, {
+      title,
+      imageURL,
+      imageAlt: title,
+      content: desc,
+    }]);
   };
 
-  deleteBlog = (id) => {
-    const { allBlogs } = this.state;
+  const deleteBlog = (id) => {
     const formattedBlog = allBlogs.filter((item, index) => (index !== id));
-    this.setState({
-      allBlogs: formattedBlog,
-    });
+    setAllBlogs(formattedBlog);
   };
 
-  render() {
-    const { allBlogs } = this.state;
-    const blogList = allBlogs.map((eachBlog, index) => (
-      <BlogCard
-        details={eachBlog}
-        id={index}
-        deleteBlog={(id) => this.deleteBlog(id)}
+  const blogList = allBlogs.map((eachBlog, index) => (
+    <BlogCard
+      details={eachBlog}
+      id={index}
+      deleteBlog={(id) => deleteBlog(id)}
+    />
+  ));
+  return (
+    <div className="app">
+      <HomePage
+        onFormSubmit={(event, content) => onFormSubmit(event, content)}
+        blogList={blogList}
+        blogImage={blogImage}
       />
-    ));
-    return (
-      <div className="app">
-        <HomePage
-          onFormSubmit={(event, content) => this.onFormSubmit(event, content)}
-          blogList={blogList}
-          blogImage={blogImage}
-        />
-      </div>
-    );
-  }
+    </div>
+  );
 }
 
 export default App;
