1dd90a9d44f4b4c6af213042976e459c7de4cb58
aditiverma1009 <aditiverma1009@gmail.com>
2020-05-31 13:05:10 +0530

    change rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,28 +1,36 @@
 module.exports = {
-  env: {
-    browser: true,
-    es6: true,
-  },
-  "parser": "babel-eslint",
-  extends: [
-    'plugin:react/recommended',
-    'airbnb',
-  ],
-  globals: {
-    Atomics: 'readonly',
-    SharedArrayBuffer: 'readonly',
-  },
-  parserOptions: {
-    ecmaFeatures: {
-      jsx: true,
+    env: {
+        browser: true,
+        es6: true,
     },
-    ecmaVersion: 2018,
-    sourceType: 'module',
-  },
-  plugins: [
-    'react',
-  ],
-  rules: {
-    'react/prefer-stateless-function': 0,
-  },
+    parser: 'babel-eslint',
+    extends: ['plugin:react/recommended', 'airbnb'],
+    globals: {
+        Atomics: 'readonly',
+        SharedArrayBuffer: 'readonly',
+    },
+    parserOptions: {
+        ecmaFeatures: {
+            jsx: true,
+        },
+        ecmaVersion: 2018,
+        sourceType: 'module',
+    },
+    plugins: ['react'],
+    rules: {
+        'react/prefer-stateless-function': 0,
+        singleQuote: true,
+        quotes: ['error', 'backtick', { else: 'single' }],
+        'comma-dangle': [
+            'error',
+            {
+                arrays: 'never',
+                objects: 'never',
+                imports: 'never',
+                exports: 'never',
+                functions: 'never',
+            },
+        ],
+        'arrow-parens': true,
+    },
 };
